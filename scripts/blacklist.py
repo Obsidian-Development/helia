86fda0930e99a832255b95ed1aec30e8d0b64721
Pań Miłosław <46422808+pieckenst@users.noreply.github.com>
2020-01-14 00:05:48 +0300

    Add files via upload

---
+++ scripts/blacklist.py
@@ -0,0 +1,1 @@
+list = ("@everyone" or "@here" or "@Пользователи")
---
+++ scripts/desAnime.py
@@ -0,0 +1,9 @@
+images = [
+"https://i.imgur.com/sFpRwYz.jpg",
+"https://i.imgur.com/RdRPMBN.jpg",
+"https://i.imgur.com/uEvf8o4.jpg",
+"https://i.imgur.com/1f9luYv.jpg",
+"https://i.imgur.com/eRJPuBk.jpg",
+"https://i.imgur.com/wOIakaH.jpg",
+"https://i.imgur.com/AE0Ph4G.jpg",
+"https://i.imgur.com/o9wzpSI.jpg"]
