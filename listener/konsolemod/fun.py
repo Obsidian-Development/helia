baaab971e37cf978c2da079b9cf1be6a28bc2e7a
Pań Miłosław <46422808+pieckenst@users.noreply.github.com>
2020-01-30 16:38:20 +0300

    cut $barany , was just put for memes

--- listener/konsolemod/fun.py
+++ listener/konsolemod/fun.py
@@ -12,24 +12,6 @@ class fun(commands.Cog):
        await ctx.send("bot: The Command Is Currently Disabled")
 
 
-    @commands.command()
-    async def barany(self, ctx):
-        embed=discord.Embed(title="Песня Про Путина И Баранов ")
-        embed.add_field(name="1", value="на российской на земле путин пас барана путин скот держал в говне как ублюдков сраных.", inline=False)
-        embed.add_field(name="2", value="но бараны дураки путина любили и за вовой дружно шли блея голосили", inline=False)
-        embed.add_field(name="3", value="путину слава так нам и надо да здравствует путин мы же не люди", inline=False)
-        embed.add_field(name="4", value="путин часто обещал накормить барана и отмыть их от говна поздно или рано", inline=False)
-        embed.add_field(name="5", value="и бараны дураки на него молились и за вовой дружно шли блея голосили путину слава", inline=False)
-        embed.add_field(name="6", value="так нам и надо да здравствует путин мы же не люди путину слава", inline=False)
-        embed.add_field(name="7", value="так нам и надо да здравствует путин мы же не люди этот гребаный пастух пас давно россию", inline=False)
-        embed.add_field(name="8", value="и баранам дуракам чудился мессия и бараны дураки путина любили и за вовой дружно шли блея голосили", inline=False)
-        embed.add_field(name="9", value="путину слава так нам и надо да здравствует путин мы же не люди", inline=False)
-        embed.add_field(name="10", value="путину слава так нам и надо да здравствует путин мы же не люди", inline=False)
-        embed.add_field(name="11", value="смысл у песни очень прост и понятен многим что в россии как всегда дураки к дороге а дорог в россии нет", inline=False)
-        embed.add_field(name="12", value="есть бараны-люди и такими дураками будет править путин путину слава так нам и надо", inline=False)
-        embed.add_field(name="13", value="да здравствует путин мы же не люди путину слава так нам и надо да здравствует путин мы же не люди", inline=False)
-        embed.add_field(name="_____", value="____", inline=True)
-        await ctx.send(embed=embed)
 
 
 
