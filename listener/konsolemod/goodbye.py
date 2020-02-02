59d665a31f09df4b3e02cdd5326c1a149abbe1fb
Andrey <rsncra@protonmail.ch>
2020-02-02 23:22:11 +0300

    db update part 2

--- listener/konsolemod/goodbye.py
+++ listener/konsolemod/goodbye.py
@@ -2,7 +2,8 @@ import discord
 import asyncio
 from datetime import datetime
 import functools
-import os
+import sqlite3
+from scripts import db
 from discord.ext import commands
 class goodbye(commands.Cog):
     def __init__(self,bot):
@@ -10,44 +11,69 @@ class goodbye(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        byetxt = os.path.join(f"db/goodbye/goodbye_{member.guild.id}.txt")
-        with open(byetxt, "r") as file:
-            set = file.read()
-            setchan = int(set)
         #now  = datetime.now()
         #time = now.strftime("%H:%M:%S")
-        gb = discord.Embed(title="User Exited The Server", description=f"{member} goodbye", color=0xf4211a)
-        #gb.add_field(name="Время", value=time, inline=False)
-        gb.set_author(name=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
-        gb.set_thumbnail(url=f"{member.avatar_url}")
-        channel = self.bot.get_channel(setchan)
+        connect = sqlite3.connect(db.main)
+        cursor = connect.cursor()
+        cursor.execute(db.select_table("goodbye", member.guild.id))
+        chan = cursor.fetchone()
+        if chan is None:
+            print(f"Server {member.guild.id} doesent have a set goodbye channel")
+            return
+        else:
+            gb = discord.Embed(title="User left the channel", description=f"{member} goodbye", color=0xf4211a)
+            #gb.add_field(name="Время", value=time, inline=False)
+            gb.set_author(name=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+            gb.set_thumbnail(url=f"{member.avatar_url}")
+            channel = self.bot.get_channel(id=int(chan[0]))
+        cursor.close()
+        connect.close()  
         await channel.send(embed=gb)
 
 
     @commands.group(invoke_without_command=True)
     async def goodbye(self, ctx):
-        await ctx.send("bot: Set A channel for farewell messages")
+        await ctx.send("bot: Set a goodbye channel")
 
     @goodbye.command()
     async def channel(self, ctx, channel: discord.TextChannel):
-        author =  ctx.message.author
-        byetxt = os.path.join(f"db/goodbye/goodbye_{author.guild.id}.txt")
-        if author.guild_permissions.administrator:
-                if os.path.exists(byetxt):
-                    os.remove(byetxt)
-                with open(byetxt,"a") as goodbye_f:
-                     goodbye_f.write(f"{channel.id}")
-                await ctx.send(f"bot: Set a farewell chanell to  {channel.mention}")
+        author =  ctx.message.author     
+        if author.guild_permissions.administrator: 
+            connect = sqlite3.connect(db.main)
+            cursor = connect.cursor()
+            cursor.execute(db.select_table("goodbye", ctx.guild.id))
+            result = cursor.fetchone()
+            if result is None:
+                val = (ctx.guild.id, channel.id)
+                cursor.execute(db.insert_table("goodbye"), val)
+            else:
+                cursor.execute(db.update_table("goodbye", channel.id, ctx.guild.id))
+            connect.commit()
+            cursor.close()
+            connect.close()
+            await ctx.send(f"bot: Succesufully set the farewell channel to {channel.mention}")
+        else:
+            await ctx.send("bot: You do not have enough permissions - :You require **Administrator**")
+
+                
     @goodbye.command()
     async def clear(self, ctx):
         author =  ctx.message.author
         if author.guild_permissions.administrator:
-            byeclear = os.path.join(f"db/goodbye/goodbye_{author.guild.id}.txt")
-            if os.path.exists(byeclear):
-                 os.remove(byeclear)
-            await ctx.send("bot: Config File Cleared")
+            connect = sqlite3.connect(db.main)
+            cursor = connect.cursor()
+            cursor.execute(db.select_table("goodbye", ctx.guild.id))
+            result = cursor.fetchone()
+            if result is None:
+                await ctx.send("bot: Do not have a table for the welcome channel - Check Database")
+            else:
+                cursor.execute(db.delete_table("goodbye", ctx.guild.id))
+                await ctx.send("bot: Cleared The Table")  
+            connect.commit()
+            cursor.close()
+            connect.close()  
         else:
-           await ctx.send("bot: Not Enough Perms. Need permission: **Administrator**")
+           await ctx.send("bot: You do not have enough permissions - :You require **Administrator**")
 
 
 def setup(bot):
