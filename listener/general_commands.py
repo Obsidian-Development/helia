20d102e02217cf8fb492b3214fbef6a9fc8cefe3
Pań Miłosław <46422808+pieckenst@users.noreply.github.com>
2020-02-21 09:06:57 +0300

    Add files via upload

--- listener/general_commands.py
+++ listener/general_commands.py
@@ -3,13 +3,13 @@ from discord.ext import commands
 
 commands_string = """
 **Moderation and Administartion Contact Commands Info**
-`clear`, `devnull`(alias `ban`), `rmmod`, `unrmmod`, `userdel`(alias `kick`), `sub`, `ticket`, `shutdown`, `welcome` , `goodbye` , `set_status`
+`clear (message count)`, `devnull @member`(alias `ban`), `rmmod @member`, `unrmmod @member`, `userdel @member`(alias `kick`), `sub create`, `ticket create`, `shutdown`, `welcome` , `goodbye` , `set_status`
 
 **Utility Commands**
-`embed` `randint` `remind` `sqrt` `wallpaper`
+`embed "your text" your text` `randint` `remind` `sqrt` `wallpaper`
 
 **Other Commands**
-`avatar` `guild` `neofetch` `voicedemo` `casino` `kubik` `monetka` `ubuntu ` `mint` `manjaro` `debian` `arch` `echo` `ping` `deepin` `setname`
+`avatar` `guild` `neofetch` `voicedemo (channel id) ` `casino` `kubik` `monetka` `ubuntu ` `mint` `manjaro` `debian` `arch` `echo (your text)` `ping` `deepin` `setname @member (your text)`
 
 **SOURCE CODE**
 "https://github.com/pieckenst/openbot"
@@ -64,13 +64,13 @@ class GeneralListener(commands.Cog):
     async def invite(self, ctx):
         embed = discord.Embed(title="Recomended Functionality Bot Invite", colour=discord.Colour(0xff6900), url="https://discordapp.com/oauth2/authorize?client_id=666304823934844938&scope=bot&permissions=2146958545", description="Bot invite Links")
         embed.set_author(name="Basic Functionality Bot Invite", url="https://discordapp.com/oauth2/authorize?client_id=666304823934844938&scope=bot&permissions=67213505")
-        embed.set_footer(icon_url="https://cdn.discordapp.com/avatars/666304823934844938/4a2b2b8de59275e0986de9de582acb25.webp?size=1024")
+        embed.set_footer(text="Openbot", icon_url="https://cdn.discordapp.com/avatars/666304823934844938/4a2b2b8de59275e0986de9de582acb25.webp?size=1024")
         await ctx.send(embed=embed)
 
     @commands.command()
     async def set_status(self, ctx, *args):
         author = ctx.message.author
-        valid_users = ["540142383270985738", "573123021598883850"]
+        valid_users = ["540142383270985738", "573123021598883850", "584377789969596416", "106451437839499264", "237984877604110336", "579750505736044574", "497406228364787717"]
         if str(author.id) in valid_users:
             await self.client.change_presence( activity=discord.Game(" ".join(args)) )
             embed=discord.Embed(title="Рапорт", description="Ваш приказ выполнен о владыка ", color=0xff8000)
