e7145e2d1abac7bf35d47d325ef4c8faca1ea156
Andrey Savich <rsncra@gmail.com>
2021-04-30 14:31:27 +0300

    more update

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -358,7 +358,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin, name='Music'):
             raise NoMoreTracks
         await player.stop()
         nextEmbed=discord.Embed(title=STRINGS['music']['skipsongtext'],colour=0xffd500)
-        nextEmbed.add_field(name=STRINGS['music']['queuenextinline'],description="\n".join(f"**{i+2}.** {t.title}"for i, t in enumerate(upcoming[:19])))
+        nextEmbed.add_field(name=STRINGS['music']['queuenextinline'],description=player.queue.current_track.title)
         nextEmbed.set_footer(text=STRINGS['music']['embed_controler_footer'])
         
         await ctx.send(embed=nextEmbed)
