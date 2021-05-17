f76429ac5e090fcb24ed940665f226fd165d4f5f
Andrey Savich <rsncra@gmail.com>
2021-05-17 22:17:32 +0300

    new help

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -22,64 +22,64 @@ class General(commands.Cog, name='General'):
         self.bot = bot
         self.name = 'General'
 
-    @commands.command()
-    @commands.guild_only()
-    async def help(self, ctx: Context, command: str = None) -> NoReturn:
-        """Shows help for a specific command, or displays a complete list of commands.
+    #@commands.command()
+    #@commands.guild_only()
+    #async def help(self, ctx: Context, command: str = None) -> NoReturn:
+        #"""Shows help for a specific command, or displays a complete list of commands.
 
-        Attributes:
-        -----------
-        - `command` - the command to display help for. 
-            If `command` is empty, displays a complete list of commands.     
-            If the command does not exist, writes that the command was not found.
+        #Attributes:
+        #-----------
+        #- `command` - the command to display help for.
+            #If `command` is empty, displays a complete list of commands.
+            #If the command does not exist, writes that the command was not found.
 
-        """
-        s = await Settings(ctx.guild.id)
-        lang = await s.get_field('locale', CONFIG['default_locale'])
-        prefix = await s.get_field('prefix', CONFIG['default_prefix'])
-        STRINGS = Strings(lang)
-        COMMANDS = Commands(lang)
+        #"""
+        #s = await Settings(ctx.guild.id)
+        #lang = await s.get_field('locale', CONFIG['default_locale'])
+        #prefix = await s.get_field('prefix', CONFIG['default_prefix'])
+        #STRINGS = Strings(lang)
+        #COMMANDS = Commands(lang)
 
-        if command == None:
-            embed = discord.Embed(
-                title=STRINGS['general']['commands_list'], description=STRINGS['general']['help_list_description'].format(prefix), color=0xef940b)
-            for i in COMMANDS:
-                title = COMMANDS[i]['title']
+        #if command == None:
+            #embed = discord.Embed(
+                #itle=STRINGS['general']['commands_list'], description=STRINGS['general']['help_list_description'].format(prefix), color=0xef940b)
+            #for i in COMMANDS:
+                #title = COMMANDS[i]['title']
 
-                description = ', '.join(
-                    [f'`{j}`' for j in COMMANDS[i]['commands']])
+                #description = ', '.join(
+                    #[f'`{j}`' for j in COMMANDS[i]['commands']])
 
-                if self.bot.get_cog(i) != None:
-                    embed.add_field(
-                        name=title, value=description, inline=False)
-            embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
-            await ctx.send(embed=embed)
+                #if self.bot.get_cog(i) != None:
+                    #embed.add_field(
+                        #name=title, value=description, inline=False)
+            #embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+            #await ctx.send(embed=embed)
 
-        elif command != '':
-            for i in COMMANDS:
-                for j in COMMANDS[i]['commands']:
-                    if command == j:
-                        embed = discord.Embed(
-                            title=STRINGS['general']['helpsystemtitle'].format(f'`{prefix}{j}`'), color=0xef940b)
+        #elif command != '':
+            #for i in COMMANDS:
+                #for j in COMMANDS[i]['commands']:
+                    #if command == j:
+                        #embed = discord.Embed(
+                            #title=STRINGS['general']['helpsystemtitle'].format(f'`{prefix}{j}`'), color=0xef940b)
 
-                        embed.add_field(
-                            name=STRINGS['general']['description'], value=COMMANDS[i]['commands'][j]['description'], inline=False)
+                        #embed.add_field(
+                            #name=STRINGS['general']['description'], value=COMMANDS[i]['commands'][j]['description'], inline=False)
 
-                        embed.add_field(
-                            name=STRINGS['general']['usage'], value=COMMANDS[i]['commands'][j]['usage'].format(prefix), inline=False)
+                        #embed.add_field(
+                            #name=STRINGS['general']['usage'], value=COMMANDS[i]['commands'][j]['usage'].format(prefix), inline=False)
 
-                        if len(COMMANDS[i]['commands'][j]['aliases']) > 0:
-                            aliases = ', '.join(
-                                [f'`{alias}`' for alias in COMMANDS[i]['commands'][j]['aliases']])
-                            embed.add_field(
-                                name=STRINGS['general']['aliases'], value=aliases, inline=False)
+                        #if len(COMMANDS[i]['commands'][j]['aliases']) > 0:
+                            #aliases = ', '.join(
+                                #[f'`{alias}`' for alias in COMMANDS[i]['commands'][j]['aliases']])
+                            #embed.add_field(
+                                #name=STRINGS['general']['aliases'], value=aliases, inline=False)
 
-                        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+                        #embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
 
-                        await ctx.send(embed=embed)
-                        return
-            else:
-                await ctx.send(embed=Utils.error_embed(STRINGS['error']['command_not_found']))
+                        #await ctx.send(embed=embed)
+                        #return
+            #else:
+                #await ctx.send(embed=Utils.error_embed(STRINGS['error']['command_not_found']))
 
     @commands.guild_only()
     @commands.command(description='Echo Commands')
@@ -122,7 +122,7 @@ class General(commands.Cog, name='General'):
             req = wikipedia.page(searcher)
             wikip = discord.Embed(title=req.title, description="Wikipedia search results", url=req.url, color=0x269926)
             wikip.set_thumbnail(url=req.images[0])
-            await ctx.send(embed=wikip)        
+            await ctx.send(embed=wikip)
         except wikipedia.exceptions.PageError:
             wikierror=discord.Embed(title="Wikipedia Error", description="Page not found or some other error")
             wikierror.add_field(name="If you are still having this error", value="Report the issue on github or ask in bot support server about it", inline=True)
---
+++ src/cogs/help.py
@@ -0,0 +1,99 @@
+from typing import Optional
+
+from discord import Embed
+from discord.utils import get
+from discord.ext.menus import MenuPages, ListPageSource
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+from cogs.utils import Logger, Settings, Config, Commands, Strings, Utils
+
+CONFIG = Config()
+
+def syntax(command):
+    cmd_and_aliases = "|".join([str(command), *command.aliases])
+    params = []
+
+    for key, value in command.params.items():
+        if key not in ("self", "ctx"):
+            params.append(f"[{key}]" if "NoneType" in str(value) else f"<{key}>")
+
+    params = " ".join(params)
+
+    return f"```{cmd_and_aliases} {params}```"
+
+
+class HelpMenu(ListPageSource):
+    def __init__(self, ctx, data):
+        self.ctx = ctx
+
+        super().__init__(data, per_page=5)
+
+    async def write_page(self, menu, fields=[]):
+        offset = (menu.current_page*self.per_page) + 1
+        len_data = len(self.entries)
+        s = await Settings(self.ctx.guild.id)
+        lang = await s.get_field('locale', CONFIG['default_locale'])
+        prefix = await s.get_field('prefix', CONFIG['default_prefix'])
+        STRINGS = Strings(lang)
+
+        embed = Embed(title=STRINGS['general']['helpsystemtitle'],
+					  description=STRINGS['general']['commands_list'].format(prefix),
+					  colour=self.ctx.author.colour)
+        #embed.set_thumbnail(url=self.ctx.guild.me.avatar_url)
+        embed.set_footer(text=f"{self.ctx.guild.me.name}: {offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} commands.", icon_url=self.ctx.guild.me.avatar_url)
+
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=False)
+
+        return embed
+
+    async def format_page(self, menu, entries):
+        fields = []
+        s = await Settings(self.ctx.guild.id)
+        lang = await s.get_field('locale', CONFIG['default_locale'])
+        prefix = await s.get_field('prefix', CONFIG['default_prefix'])
+        STRINGS = Strings(lang)
+        COMMANDS = Commands(lang)
+
+        for entry in entries:
+            fields.append((STRINGS['general']['nocommanddescription'], syntax(entry)))
+        return await self.write_page(menu, fields)
+
+
+class Help(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.remove_command("help")
+
+    async def cmd_help(self, ctx, command):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field('locale', CONFIG['default_locale'])
+        prefix = await s.get_field('prefix', CONFIG['default_prefix'])
+        STRINGS = Strings(lang)
+        embed = Embed(title=f"Help with `{command}`",description=syntax(command),colour=ctx.author.colour)
+        embed.add_field(name=STRINGS['general']['description'], value=command.help)
+        await ctx.send(embed=embed)
+
+    @command(name="help")
+    async def show_help(self, ctx, cmd: Optional[str]):
+        """Shows this message."""
+        if cmd is None:
+            menu = MenuPages(source=HelpMenu(ctx, list(self.bot.commands)),
+                             delete_message_after=True,
+                             timeout=60.0)
+            await menu.start(ctx)
+
+        else:
+            if (command := get(self.bot.commands, name=cmd)):
+                await self.cmd_help(ctx, command)
+            else:
+                await ctx.send("That command does not exist.")
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("help")
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
