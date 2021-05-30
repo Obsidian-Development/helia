345237f25d6c7b22b9203db049bd7e58826b043d
Restyled.io <commits@restyled.io>
2021-05-30 17:50:07 +0000

    Restyled by reorder-python-imports

--- src/main.py
+++ src/main.py
@@ -9,27 +9,29 @@
 # *
 # * SPDX-License-Identifier: EPL-2.0 OR MPL-2.0
 # ********************************************************************************/
-
-
 import asyncio
 import json
 import os
 import random
 import sqlite3
-from os.path import abspath, dirname
+from os.path import abspath
+from os.path import dirname
 from typing import NoReturn
 
 import discord
 import requests
-# from scripts import db # UNCOMMENT FOR DB CONNECTION
-from discord.ext import commands, tasks
+from cogs.utils import Config
+from cogs.utils import Logger
+from cogs.utils import Strings
+from cogs.utils import Utils
+from discord.ext import commands
+from discord.ext import tasks
 from discord.ext.commands import AutoShardedBot
 from discord_slash import SlashCommand
 from dotenv import load_dotenv
 from slashify import Slashify
 from termcolor import cprint
-
-from cogs.utils import Config, Logger, Strings, Utils
+# from scripts import db # UNCOMMENT FOR DB CONNECTION
 
 loaded = False
 
