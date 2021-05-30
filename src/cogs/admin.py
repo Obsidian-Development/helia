e6fd194160664ab98a4d2aa803ef6464b5b90fe2
Restyled.io <commits@restyled.io>
2021-05-30 19:52:10 +0000

    Restyled by autopep8

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -16,6 +16,7 @@ STRINGS = Strings(CONFIG["default_locale"])
 
 class Admin(commands.Cog, name="Admin"):
     """A module required to administer the bot. Only works for its owners."""
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.name = "Admin"
@@ -166,20 +167,17 @@ class Admin(commands.Cog, name="Admin"):
         embed = discord.Embed(
             title=STRINGS["general"]["botinvitetitle"],
             colour=discord.Colour(0xFF6900),
-            url=
-            f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=204859462&scope=applications.commands%20bot",
+            url=f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=204859462&scope=applications.commands%20bot",
             description=STRINGS["general"]["botinvitedesc"],
         )
         embed.set_author(
             name=STRINGS["general"]["botinvitedescd"],
-            url=
-            f"https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=204557314",
+            url=f"https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=204557314",
         )
         # mostly useful for helia canary invite but still why not have it be there - comment if your self hosted version will not have canary branch
         embed.add_field(
             name=STRINGS["general"]["canaryver"],
-            value=
-            f"https://discord.com/oauth2/authorize?client_id=671612079106424862&scope=bot&permissions=204557314",
+            value=f"https://discord.com/oauth2/authorize?client_id=671612079106424862&scope=bot&permissions=204557314",
             inline=False,
         )
         embed.add_field(
