c337fcd3db136a100e102f2ddbcc134a1e06bbf2
Restyled.io <commits@restyled.io>
2021-05-30 17:50:04 +0000

    Restyled by black

--- src/main.py
+++ src/main.py
@@ -27,6 +27,7 @@ from dotenv import load_dotenv
 import asyncio
 import requests
 import sqlite3
+
 # from scripts import db # UNCOMMENT FOR DB CONNECTION
 from discord.ext import tasks, commands
 from discord_slash import SlashCommand
@@ -50,63 +51,69 @@ class Helia(commands.AutoShardedBot):
         )
 
         CONFIG = Config()
-        STRINGS = Strings(CONFIG['default_locale'])
+        STRINGS = Strings(CONFIG["default_locale"])
         self.filepath = dirname(abspath(__file__))
         self.statuses = [
             # discord.Activity(type=discord.ActivityType.watching, name=f"{len(self.bot.guilds)} servers | {len(self.bot.shards)} shards!"), # bugged status - uncomment after fix
-            discord.Activity(type=discord.ActivityType.watching,
-                             name="Ping me for prefix"),
-            discord.Activity(type=discord.ActivityType.listening,
-                             name="Dont forget to bump the bot every 3 hours on bot lists!"),
-            discord.Game(name=f"{self.command_prefix}help for info"),
-            discord.Activity(type=discord.ActivityType.listening,
-                             name="to my creator Middlle#7488"),
-            discord.Game(name="Final Fantasy XIV"),
-            discord.Activity(type=discord.ActivityType.watching,
-                             name="our support server https://discord.gg/7uUBM8mKbu"),
-            discord.Game(
-                name="Deep inside, we're nothing more than scions and sinners"),
             discord.Activity(
-                type=discord.ActivityType.watching, name="headbanging")
+                type=discord.ActivityType.watching, name="Ping me for prefix"
+            ),
+            discord.Activity(
+                type=discord.ActivityType.listening,
+                name="Dont forget to bump the bot every 3 hours on bot lists!",
+            ),
+            discord.Game(name=f"{self.command_prefix}help for info"),
+            discord.Activity(
+                type=discord.ActivityType.listening, name="to my creator Middlle#7488"
+            ),
+            discord.Game(name="Final Fantasy XIV"),
+            discord.Activity(
+                type=discord.ActivityType.watching,
+                name="our support server https://discord.gg/7uUBM8mKbu",
+            ),
+            discord.Game(
+                name="Deep inside, we're nothing more than scions and sinners"
+            ),
+            discord.Activity(type=discord.ActivityType.watching, name="headbanging"),
         ]
 
         load_dotenv()
         self.TOKEN = os.getenv("DISCORD_TOKEN")
 
-        cprint(STRINGS['etc']['info']['art'], 'white')
-        cprint('Default locale is {0}'.format(
-            CONFIG['default_locale']), 'green'
-        )
+        cprint(STRINGS["etc"]["info"]["art"], "white")
+        cprint("Default locale is {0}".format(CONFIG["default_locale"]), "green")
 
         self.slash = SlashCommand(self, override_type=True)
         Slashify(self)
         global loaded
 
-        if not loaded:  # using this so the bot doesn't initialize a second time when trying to get variables or functions
+        if (
+            not loaded
+        ):  # using this so the bot doesn't initialize a second time when trying to get variables or functions
             print("Loading cogs:")
-            for filename in os.listdir(self.filepath + '/cogs'):
-                if filename.endswith('.py'):
+            for filename in os.listdir(self.filepath + "/cogs"):
+                if filename.endswith(".py"):
                     try:
-                        self.load_extension('cogs.{0}'.format(filename[:-3]))
+                        self.load_extension("cogs.{0}".format(filename[:-3]))
                         print(f"    Loaded '{filename}'")
                     except Exception as e:
                         print(str(e))
-            self.load_extension('jishaku')
+            self.load_extension("jishaku")
             print("    Loaded 'jishaku.py'")
             loaded = True
 
-# uncomment the code below if you want to load cogs from folders that are in the cog folder
-# this is used too keep things organised and see what belongs to what
+    # uncomment the code below if you want to load cogs from folders that are in the cog folder
+    # this is used too keep things organised and see what belongs to what
 
-#                 elif not "." in filename and os.path.isdir(self.filepath + "/cogs/" + filename):
-#                     print(f"    Loading cogs from '{filename}:'")
-#                     for filename1 in os.listdir(self.filepath + "/cogs/" + filename):
-#                         if filename1.endswith(".py"):
-#                             try:
-#                                 self.load_extension(f"cogs.{filename}.{filename1}")
-#                                 print(f"        Loaded '{filename1}'")
-#                             except Exception as e1:
-#                                 print(str(e1))
+    #                 elif not "." in filename and os.path.isdir(self.filepath + "/cogs/" + filename):
+    #                     print(f"    Loading cogs from '{filename}:'")
+    #                     for filename1 in os.listdir(self.filepath + "/cogs/" + filename):
+    #                         if filename1.endswith(".py"):
+    #                             try:
+    #                                 self.load_extension(f"cogs.{filename}.{filename1}")
+    #                                 print(f"        Loaded '{filename1}'")
+    #                             except Exception as e1:
+    #                                 print(str(e1))
 
     @tasks.loop(seconds=80)
     async def changeStatus(self):
@@ -114,7 +121,9 @@ class Helia(commands.AutoShardedBot):
         print("---------------------------")
         print("[DYNAMIC-STATUS] Dynamic status changed")
         print("---------------------------")
-        await self.change_presence(status=discord.Status.online, activity=random.choice(self.statuses))
+        await self.change_presence(
+            status=discord.Status.online, activity=random.choice(self.statuses)
+        )
 
     async def on_connect(self):
         print("[CONNECTION] Connected to the Discord API")
@@ -134,12 +143,10 @@ def add_to_guild(access_token, userID):
     url = f"{Oauth.discord_api_url}/guilds/{816985615811608616}/members/{userID}"
     headers = {
         "Authorization": f"Bot {access_token}",
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
     }
 
-    data = {
-        "access_token": access_token
-    }
+    data = {"access_token": access_token}
 
     response = requests.put(url=url, json=data, headers=headers)
     print(response.text)
