425a3d8690235c5d8c09da3e2a515570a3ead3dc
Andrey Savich <rsncra@gmail.com>
2021-06-07 18:37:05 +0300

    gnulinux

--- src/cogs/gnulinux.py
+++ src/cogs/gnulinux.py
@@ -5,6 +5,11 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashContext, cog_ext
 
+from cogs.utils import Config, Logger, Settings, Strings
+
+CONFIG = Config()
+
+
 
 class gnulinux(commands.Cog):
     def __init__(self, bot):
@@ -13,52 +18,47 @@ class gnulinux(commands.Cog):
     # GNU/Linux Distr Wiki
     @commands.command(description="Arch Linux Description")
     async def arch(self, ctx: SlashContext):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         archl = discord.Embed(
             title="Arch Linux",
             url="https://www.archlinux.org/download/",
-            description=
-            "Arch Linux is an independent general purpose GNU / Linux distribution optimized for the x86-64 architecture, which aims to provide the latest stable versions of software following the rolling release model. By default, the user is provided with a minimalistic basic system in which the user can add what he needs. Pacman package manager is used to install, uninstall and update packages.",
+            description=STRINGS["gnulinuxx"]["archdesc"],
             color=0x1793D1,
         )
         archl.set_thumbnail(
             url=
             "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Archlinux-vert-dark.svg/1280px-Archlinux-vert-dark.svg.png"
         )
-        archl.add_field(name="Suggestions",
-                        value="Install it yourself without gui",
-                        inline=True)
         await ctx.send(embed=archl)
 
     @commands.command(description="Ubuntu linux description")
     async def ubuntu(self, ctx: SlashContext):  # Ubuntu
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         ubuntu1 = discord.Embed(
             title="Ubuntu",
             url="https://ubuntu.com/",
-            description=
-            "Ubuntu is an operating system based on Debian GNU / Linux. The main developer and sponsor is Canonical. Currently, the project is actively developed and supported by the free community.",
+            description=STRINGS["gnulinuxx"]["ubuntudesc"],
             color=0xDE4714,
-        )
-        ubuntu1.add_field(
-            name="Assesement",
-            value="Good for users trying linux first time",
-            inline=True,
         )
         ubuntu1.set_thumbnail(url="https://i.imgur.com/TfVgK1v.png")
         await ctx.send(embed=ubuntu1)
 
     @commands.command(description="Debian Linux Description")
     async def debian(self, ctx: SlashContext):  # Debian
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         debian1 = discord.Embed(
             title="Debian",
             url="https://www.debian.org/",
-            description=
-            "Debian is an open source operating system. Currently, Debian GNU / Linux is one of the most popular and important GNU / Linux distributions, which in its primary form has had a significant impact on the development of this type of OS as a whole. There is also a project based on a different kernel: Debian GNU / Hurd. Debian can be used as an operating system for both servers and workstations.",
+            description=STRINGS["gnulinuxx"]["debiandesc"],
             color=0xD80150,
         )
         debian1.set_thumbnail(
@@ -69,13 +69,14 @@ class gnulinux(commands.Cog):
 
     @commands.command(description="Deepin linux description")
     async def deepin(self, ctx: SlashContext):  # Deepin
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         deepin1 = discord.Embed(
             title="Deepin",
             url="https://www.deepin.org",
-            description=
-            "Deepin (stylized as deepin; formerly known as Linux Deepin and Hiweed Linux) is a Linux distribution based on Debian's stable branch. It features DDE, the Deepin Desktop Environment, built on Qt and available for various distributions like Manjaro, Arch or Fedora. As of version 15.10 it also uses dde-kwin, a set of patches for KDE Plasma's window manager. In 2019, Huawei started to ship Linux laptops pre-installed with deepin. ",
+            description=STRINGS["gnulinuxx"]["deepindesc"],
             color=0x1793D1,
         )
         deepin1.set_thumbnail(
@@ -86,39 +87,35 @@ class gnulinux(commands.Cog):
 
     @commands.command(description="Manjaro linux description")
     async def manjaro(self, ctx: SlashContext):  # Manjaro
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         manjaro1 = discord.Embed(
             title="Manjaro",
             url="https://manjaro.org/",
-            description=
-            "Manjaro Linux or Manjaro is an Arch Linux GNU / Linux distribution that uses the rolling release update model. Several versions are officially available: with the XFCE, KDE, or GNOME desktop.",
+            description=STRINGS["gnulinuxx"]["manjarodesc"],
             color=0x35BF5C,
         )
         manjaro1.set_thumbnail(
             url=
             "https://fost.ws/uploads/posts/2019-05/1557568788_manjaro-logo.png"
         )
-        manjaro1.add_field(name="Suggestions",
-                           value="Good for not complete noobs",
-                           inline=True)
         await ctx.send(embed=manjaro1)
 
     @commands.command(description="Linux Mint description")
     async def mint(self, ctx: SlashContext):  # Mint
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
         channel = ctx.message.channel
-        await ctx.send("bot: It Will Be Done - Command Result Below")
         mint1 = discord.Embed(
             title="Linux Mint",
             url="https://linuxmint.com/",
-            description=
-            "Linux Mint is a community-developed free Linux distribution based on Ubuntu and Debian that aims to provide the user with a “modern, elegant and user-friendly operating system that is both powerful and easy to use.” Linux Mint provides full support for a variety of multimedia formats, includes some proprietary programs, and comes with an extensive set of open source applications. The founder of the project is Clement Lefebvre, the development team (Mint Linux Team) and the user community are also actively involved in the development. ",
+            description=STRINGS["gnulinuxx"]["mintdesc"],
             color=0xB1EA77,
         )
         mint1.set_thumbnail(url="https://i.imgur.com/cyRjcbp.png")
-        mint1.add_field(name="Commentary",
-                        value="For housewives and noobs",
-                        inline=True)
         await ctx.send(embed=mint1)
 
 
