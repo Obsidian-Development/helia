f2985cfe0b56ec64b8452d92e20a9db5ea533571
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-06-26 14:52:32 +0300

    Add files via upload

---
+++ src/scripts/calculator.py
@@ -0,0 +1,59 @@
+
+import math
+from discord_components import Button, ButtonStyle
+
+buttons = [
+    [
+        Button(style=ButtonStyle.grey, label="1"),
+        Button(style=ButtonStyle.grey, label="2"),
+        Button(style=ButtonStyle.grey, label="3"),
+        Button(style=ButtonStyle.blue, label="×"),
+        Button(style=ButtonStyle.red, label="Exit"),
+    ],
+    [
+        Button(style=ButtonStyle.grey, label="4"),
+        Button(style=ButtonStyle.grey, label="5"),
+        Button(style=ButtonStyle.grey, label="6"),
+        Button(style=ButtonStyle.blue, label="÷"),
+        Button(style=ButtonStyle.red, label="←"),
+    ],
+    [
+        Button(style=ButtonStyle.grey, label="7"),
+        Button(style=ButtonStyle.grey, label="8"),
+        Button(style=ButtonStyle.grey, label="9"),
+        Button(style=ButtonStyle.blue, label="+"),
+        Button(style=ButtonStyle.red, label="Clear"),
+    ],
+    [
+        Button(style=ButtonStyle.grey, label="00"),
+        Button(style=ButtonStyle.grey, label="0"),
+        Button(style=ButtonStyle.grey, label="."),
+        Button(style=ButtonStyle.blue, label="-"),
+        Button(style=ButtonStyle.green, label="="),
+    ],
+    [
+        Button(style=ButtonStyle.grey, label="("),
+        Button(style=ButtonStyle.grey, label=")"),
+        Button(style=ButtonStyle.grey, label="π"),
+        Button(style=ButtonStyle.grey, label="x²"),
+        Button(style=ButtonStyle.grey, label="x³"),
+    ],
+]
+
+
+
+
+def calculate(exp):
+    o = exp.replace("×", "*")
+    o = o.replace("÷", "/")
+    o = o.replace("π", str(math.pi))
+    o = o.replace("²", "**2")
+    o = o.replace("³", "**3")
+    result = ""
+    try:
+        result = str(eval(o))
+
+    except BaseException:
+        result = "An error occurred."
+
+    return result
