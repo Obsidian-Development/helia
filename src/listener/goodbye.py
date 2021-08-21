bc0a320b252eb2f61139297a0a4c7165b627b9ff
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-08-21 23:21:24 +0300

    do not break it

--- src/listener/goodbye.py
+++ src/listener/goodbye.py
@@ -28,25 +28,18 @@ class goodbye(commands.Cog):
             chan = cursor.fetchone()
             if chan is None:
                 return
-            cursor.execute(
-                db.select_table("goodbye", "text", "guild_id",
-                                member.guild.id))
-            desc = cursor.fetchone()
-            if desc is None:
-                desc = f"{MEMBER} goodbye "
-            gb = discord.Embed(
-                title="User Left The Channel",
-                description=(desc[0]).format(MEMBER=member,
-                                             MENTION=member.mention),
-                color=0xF4211A,
-            )
-            # gb.add_field(name="Время", value=time, inline=False)
-            gb.set_author(name=f"{member.guild}",
-                          icon_url=f"{member.guild.icon_url}")
-            gb.set_thumbnail(url=f"{member.avatar_url}")
-            channel = self.bot.get_channel(id=int(chan[0]))
+            else:
+                cursor.execute(db.select_table("goodbye", "text", "guild_id", member.guild.id))
+                desc = cursor.fetchone()
+                if desc is None: 
+                    desc = f"{MEMBER} goodbye "
+                gb = discord.Embed(title="User Left The Channel", description=(desc[0]).format(MEMBER=member, MENTION=member.mention), color=0xf4211a)
+                #gb.add_field(name="Время", value=time, inline=False)
+                gb.set_author(name=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+                gb.set_thumbnail(url=f"{member.avatar_url}")
+                channel = self.bot.get_channel(id=int(chan[0]))
             cursor.close()
-            connect.close()
+            connect.close()  
             await channel.send(embed=gb)
         except:
             print(
