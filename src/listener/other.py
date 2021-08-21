2192fbe7df608d30b3eea50a7e62babab998d2ef
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-08-21 20:58:20 +0300

    0.7
    Rework of the bot to a bit diffrent structure

---
+++ src/listener/other.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from typing import NoReturn
+
+from discord.ext import commands
+from discord.ext.commands import Bot, Context
+
+from listener.utils import Config, Logger, Settings, Strings, Utils
+
+from scripts import desAnime, desNature, desStarwars
+
+CONFIG = Config()
+
+
+class Other(commands.Cog, name="Other"):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+        self.name = "Other"
+
+    @commands.command()
+    @commands.guild_only()
+    async def ping(self, ctx: Context) -> NoReturn:
+        """Shows host latency."""
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
+        latency = int(round(self.bot.latency * 100, 1))
+
+        embed = Utils.done_embed(STRINGS["other"]["pong"].format(str(latency)))
+        await ctx.send(embed=embed)
+
+    @commands.group(invoke_without_command=True)
+    async def wallpaper(self, ctx: Context):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
+        wallinfo = discord.Embed(
+            title=STRINGS["wallpaper"]["wallpaperembedtitle"],
+            description=STRINGS["wallpaper"]["wallpaperdesc"],
+            color=0x00FF00,
+        )
+        wallinfo.add_field(
+            name=STRINGS["wallpaper"]["wallpaperusageanimetitle"],
+            value="``wallpaper anime``",
+            inline=True,
+        )
+        wallinfo.add_field(
+            name=STRINGS["wallpaper"]["wallpaperusagenaturetitle"],
+            value="``wallpaper nature``",
+            inline=True,
+        )
+        wallinfo.add_field(
+            name=STRINGS["wallpaper"]["wallpaperusagestarwarstitle"],
+            value="``wallpaper starwars``",
+            inline=True,
+        )
+        await ctx.send(embed=wallinfo)
+
+    @wallpaper.command()
+    async def anime(self, ctx: Context):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
+        embedanime = discord.Embed(
+            title=STRINGS["wallpaper"]["wallpaperanimetitle"],
+            color=0x00FF00,
+        )
+        embedanime.set_footer(
+            text=STRINGS["wallpaper"]["wallpaperanimefooter"])
+        await ctx.send(embed=embedanime)
+
+    @wallpaper.command()
+    async def nature(self, ctx: Context):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
+        imgnat = random.choice(desNature.images)
+        embednat = discord.Embed(
+            title=STRINGS["wallpaper"]["wallpapernaturetitle"],
+            color=0x00FF00,
+            url=imgnat,
+        )
+        embednat.set_image(url=imgnat)
+        await ctx.send(embed=embednat)
+
+    @wallpaper.command()
+    async def starwars(self, ctx: Context):
+        s = await Settings(ctx.guild.id)
+        lang = await s.get_field("locale", CONFIG["default_locale"])
+        STRINGS = Strings(lang)
+        imgstarwars = random.choice(desStarwars.images)
+        embedstarwars = discord.Embed(
+            title=STRINGS["wallpaper"]["wallpaperstarwarstitle"],
+            color=0x00FF00,
+            url=imgstarwars,
+        )
+        embedstarwars.set_image(url=imgstarwars)
+        await ctx.send(embed=embedstarwars)
+
+
+def setup(bot: Bot) -> NoReturn:
+    bot.add_cog(Other(bot))
+    Logger.cog_loaded(bot.get_cog("Other").name)
---
+++ src/listener/welcome.py
@@ -0,0 +1,123 @@
+# remove .removethisforenable in file name for this cog to load
+import discord
+import asyncio
+import functools
+import os
+import sqlite3
+from scripts import db
+from discord.ext import commands
+from discord.ext.commands import Bot, Context
+class welcome(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            connect = sqlite3.connect(db.main)
+            cursor = connect.cursor()
+            cursor.execute(db.select_table("welcome", "channel_id", "guild_id", member.guild.id)) 
+            chan = cursor.fetchone()
+            if chan is None:
+                return
+            else:
+                cursor.execute(db.select_table("welcome", "text", "guild_id", member.guild.id))
+                desc = cursor.fetchone()
+                if desc is None: 
+                    desc = f" Hi there {MEMBER} and welcome to our humble community"
+                hello = discord.Embed(title="Hello there", description=(desc[0]).format(MEMBER=member, MENTION=member.mention), color=0x00ff00)
+                hello.set_author(name=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+                hello.set_thumbnail(url=f"{member.avatar_url}")
+                channel = self.bot.get_channel(id=int(chan[0]))
+                await channel.send(embed=hello)      
+            cursor.close()
+            connect.close()
+        except:
+            print(f"The server  {member.guild.id} encountered an unknown error. Perhaps the welcome channel was removed.")
+
+       
+
+
+        
+    @commands.group(invoke_without_command=True)
+    async def welcome(self, ctx: Context):
+        await ctx.send("No help information temporarily")
+
+    @welcome.command(pass_context=True)
+    async def channel(self, ctx: Context, chan: discord.TextChannel=None):
+        try:
+            author = ctx.message.author     
+            if author.guild_permissions.manage_channels:
+                connect = sqlite3.connect(db.main)
+                cursor = connect.cursor()   
+                cursor.execute(db.select_table("welcome", "channel_id", "guild_id", ctx.guild.id)) 
+                res = cursor.fetchone()
+                if res is None:
+                    val = (ctx.guild.id, chan.id)
+                    cursor.execute(db.insert_table("welcome","guild_id","channel_id"), val)
+                else:
+                    cursor.execute(db.update_table("welcome", "channel_id", chan.id, "guild_id", ctx.guild.id))  
+                connect.commit()
+                cursor.close()
+                connect.close()
+                await ctx.send(f"bot: Set the welcome channel to {chan.mention}")  
+            else:
+                await ctx.send("You do not have enough permissions - :You require **Manage Channels**.")  
+        except:
+            await ctx.send("bot: Error")    
+
+
+    @welcome.command(pass_context=True)
+    async def clear(self, ctx: Context):
+        try:
+            author = ctx.message.author
+            if author.guild_permissions.manage_channels:
+                connect = sqlite3.connect(db.main)
+                cursor = connect.cursor()
+                cursor.execute(db.select_table("welcome", "channel_id", "guild_id", ctx.guild.id))
+                res = cursor.fetchone()
+                if res is None:
+                    await ctx.send("bot: Do not have a table for the welcome channel - Check Database.")
+                else:
+                    cursor.execute(db.delete_table("welcome", "guild_id", ctx.guild.id))
+                    await ctx.send("bot: Cleared the table")
+                connect.commit()
+                cursor.close()
+                connect.close()
+            else:
+                await ctx.send("You do not have enough permissions - :You require **Manage Channels**.")
+        except:
+            await ctx.send("bot: Error")
+    
+    @welcome.command(pass_context=True)
+    async def text(self, ctx: Context,*,content=None):
+        try:    
+            author = ctx.message.author
+            if author.guild_permissions.manage_channels:
+                if content is None:
+                    return await ctx.send("bot: Please type the text you wish for the welcome message")
+                connect = sqlite3.connect(db.main)
+                cursor = connect.cursor()
+                cursor.execute(db.select_table("welcome", "text", "guild_id", ctx.guild.id))
+                res = cursor.fetchone()
+                if res is None:
+                    val = (ctx.guild.id, content)
+                    cursor.execute(db.insert_table("welcome","guild_id","text"), val)
+                else:
+                    val = (content, ctx.guild.id)
+                    cursor.execute("UPDATE welcome SET text = ? WHERE guild_id = ?", val)      
+                connect.commit()
+                cursor.close()
+                connect.close()
+                await ctx.send(f"bot: Set the welcome message text") 
+            else:
+                await ctx.send("bot: You do not have enough permissions - :You require **Manage Channels**.")
+        except:
+            await ctx.send("bot: Error , argument may be invalid")
+
+
+
+
+       
+def setup(bot):
+    bot.add_cog(welcome(bot))
