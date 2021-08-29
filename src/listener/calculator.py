7e4739b3ce5290482bd9fe6c4696773d18be2c12
Restyled.io <commits@restyled.io>
2021-08-29 19:02:43 +0000

    Restyled by black

--- src/listener/calculator.py
+++ src/listener/calculator.py
@@ -45,9 +45,11 @@ class Calculator(commands.Cog, name="Calculator"):
             color=discord.Colour.blurple(),
         )
         await m.edit(content="", components=buttons, embed=e)
-        done = [[
-            Button(style=ButtonStyle.grey, label="·", disabled=True),
-        ]]
+        done = [
+            [
+                Button(style=ButtonStyle.grey, label="·", disabled=True),
+            ]
+        ]
         allowed = [
             "1",
             "2",
@@ -69,8 +71,10 @@ class Calculator(commands.Cog, name="Calculator"):
         ]
         while m.created_at < delta:
             res = await self.bot.wait_for("button_click")
-            if (res.author.id == ctx.author.id
-                    and res.message.embeds[0].timestamp < delta):
+            if (
+                res.author.id == ctx.author.id
+                and res.message.embeds[0].timestamp < delta
+            ):
                 expression = res.message.embeds[0].description[6:-3]
                 if expression in ["None", "An error occurred."]:
                     expression = ""
@@ -102,14 +106,16 @@ class Calculator(commands.Cog, name="Calculator"):
                         components=done,
                     )
 
-                elif (len(expression) > 9 or expression.count("²") >= 4
-                      or expression.count("³") >= 4
-                      or expression.count("²²") > 1
-                      or expression.count("³³") > 1
-                      or expression.count("²²³³") >= 1):
+                elif (
+                    len(expression) > 9
+                    or expression.count("²") >= 4
+                    or expression.count("³") >= 4
+                    or expression.count("²²") > 1
+                    or expression.count("³³") > 1
+                    or expression.count("²²³³") >= 1
+                ):
                     if res.component.label in allowed:
-                        await m.edit(
-                            content="Preparing to tear down the buttons")
+                        await m.edit(content="Preparing to tear down the buttons")
 
                         await res.respond(
                             type=7,
@@ -122,8 +128,7 @@ class Calculator(commands.Cog, name="Calculator"):
                         )
                         break
                     elif expression.count("××") > 1:
-                        await m.edit(
-                            content="Preparing to tear down the buttons")
+                        await m.edit(content="Preparing to tear down the buttons")
 
                         await res.respond(
                             type=7,
@@ -146,10 +151,7 @@ class Calculator(commands.Cog, name="Calculator"):
                         timestamp=delta,
                         color=discord.Colour.blurple(),
                     )
-                    await res.respond(content="",
-                                      embed=f,
-                                      components=buttons,
-                                      type=7)
+                    await res.respond(content="", embed=f, components=buttons, type=7)
 
 
 def setup(bot):
