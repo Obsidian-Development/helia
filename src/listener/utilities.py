d1f759c5659f81fd7c83722aa51559f613c3543a
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-08-29 20:47:12 +0300

    edit guild

--- src/listener/utilities.py
+++ src/listener/utilities.py
@@ -216,32 +216,124 @@ class Utilities(commands.Cog):
 
     @commands.command(aliases=["server"])
     @commands.guild_only()
-    async def guild(self, ctx: Context) -> NoReturn:
+    async def guild(self, ctx, *, guild_id: int = None) -> NoReturn:
         """Shows guild information."""
         s = await Settings(ctx.guild.id)
         lang = await s.get_field("locale", CONFIG["default_locale"])
         STRINGS = Strings(lang)
 
-        member = ctx.message.author
-        owner = str(ctx.guild.owner)
-        servinfo = discord.Embed(title="Server Info", color=0x00FF00)
-        servinfo.set_author(name=f"{member.guild}",
-                            icon_url=f"{member.guild.icon_url}")
-        servinfo.add_field(name="Region",
-                           value=f"{member.guild.region}",
-                           inline=True)
-        servinfo.set_thumbnail(url=f"{member.guild.icon_url}")
-        servinfo.add_field(name="Owner", value=f"{owner}", inline=True)
-        servinfo.add_field(
-            name="Verification Level",
-            value=f"{member.guild.verification_level}",
-            inline=True,
+        if guild_id is not None and await self.bot.is_owner(ctx.author):
+            guild = self.bot.get_guild(guild_id)
+            if guild is None:
+                return await ctx.send(f'Invalid Guild ID given.')
+        else:
+            guild = ctx.guild
+
+        roles = [role.name.replace('@', '@\u200b') for role in guild.roles]
+
+        if not guild.chunked:
+            async with ctx.typing():
+                await guild.chunk(cache=True)
+
+        # figure out what channels are 'secret'
+        everyone = guild.default_role
+        everyone_perms = everyone.permissions.value
+        secret = Counter()
+        totals = Counter()
+        for channel in guild.channels:
+            allow, deny = channel.overwrites_for(everyone).pair()
+            perms = discord.Permissions((everyone_perms & ~deny.value) | allow.value)
+            channel_type = type(channel)
+            totals[channel_type] += 1
+            if not perms.read_messages:
+                secret[channel_type] += 1
+            elif isinstance(channel, discord.VoiceChannel) and (not perms.connect or not perms.speak):
+                secret[channel_type] += 1
+
+        e = discord.Embed()
+        e.title = guild.name
+        e.description = f'**ID**: {guild.id}\n**Owner**: {guild.owner}'
+        if guild.icon:
+            e.set_thumbnail(url=guild.icon.url)
+
+        channel_info = []
+        key_to_emoji = {
+            discord.TextChannel: '<:text_channel:586339098172850187>',
+            discord.VoiceChannel: '<:voice_channel:586339098524909604>',
+        }
+        for key, total in totals.items():
+            secrets = secret[key]
+            try:
+                emoji = key_to_emoji[key]
+            except KeyError:
+                continue
+
+            if secrets:
+                channel_info.append(f'{emoji} {total} ({secrets} locked)')
+            else:
+                channel_info.append(f'{emoji} {total}')
+
+        info = []
+        features = set(guild.features)
+        all_features = {
+            'PARTNERED': 'Partnered',
+            'VERIFIED': 'Verified',
+            'DISCOVERABLE': 'Server Discovery',
+            'COMMUNITY': 'Community Server',
+            'FEATURABLE': 'Featured',
+            'WELCOME_SCREEN_ENABLED': 'Welcome Screen',
+            'INVITE_SPLASH': 'Invite Splash',
+            'VIP_REGIONS': 'VIP Voice Servers',
+            'VANITY_URL': 'Vanity Invite',
+            'COMMERCE': 'Commerce',
+            'LURKABLE': 'Lurkable',
+            'NEWS': 'News Channels',
+            'ANIMATED_ICON': 'Animated Icon',
+            'BANNER': 'Banner',
+        }
+
+        for feature, label in all_features.items():
+            if feature in features:
+                info.append(f'{ctx.tick(True)}: {label}')
+
+        if info:
+            e.add_field(name='Features', value='\n'.join(info))
+
+        e.add_field(name='Channels', value='\n'.join(channel_info))
+
+        if guild.premium_tier != 0:
+            boosts = f'Level {guild.premium_tier}\n{guild.premium_subscription_count} boosts'
+            last_boost = max(guild.members, key=lambda m: m.premium_since or guild.created_at)
+            if last_boost.premium_since is not None:
+                boosts = f'{boosts}\nLast Boost: {last_boost} ({time.format_relative(last_boost.premium_since)})'
+            e.add_field(name='Boosts', value=boosts, inline=False)
+
+        bots = sum(m.bot for m in guild.members)
+        fmt = f'Total: {guild.member_count} ({formats.plural(bots):bot})'
+
+        e.add_field(name='Members', value=fmt, inline=False)
+        e.add_field(name='Roles', value=', '.join(roles) if len(roles) < 10 else f'{len(roles)} roles')
+
+        emoji_stats = Counter()
+        for emoji in guild.emojis:
+            if emoji.animated:
+                emoji_stats['animated'] += 1
+                emoji_stats['animated_disabled'] += not emoji.available
+            else:
+                emoji_stats['regular'] += 1
+                emoji_stats['disabled'] += not emoji.available
+
+        fmt = (
+            f'Regular: {emoji_stats["regular"]}/{guild.emoji_limit}\n'
+            f'Animated: {emoji_stats["animated"]}/{guild.emoji_limit}\n'
         )
-        servinfo.add_field(name="Member Count",
-                           value=f"{member.guild.member_count}",
-                           inline=True)
-        servinfo.add_field(name="ID", value=f"{member.guild.id}", inline=True)
-        await ctx.send(embed=servinfo)
+        if emoji_stats['disabled'] or emoji_stats['animated_disabled']:
+            fmt = f'{fmt}Disabled: {emoji_stats["disabled"]} regular, {emoji_stats["animated_disabled"]} animated\n'
+
+        fmt = f'{fmt}Total Emoji: {len(guild.emojis)}/{guild.emoji_limit*2}'
+        e.add_field(name='Emoji', value=fmt, inline=False)
+        e.set_footer(text='Created').timestamp = guild.created_at
+        await ctx.send(embed=e)
 
 
 def setup(bot: Bot) -> NoReturn:
