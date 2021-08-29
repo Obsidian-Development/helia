c47c87440127b35066dceace91234ba3607b0425
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-08-29 21:58:50 +0300

    fix

--- src/listener/calculator.py
+++ src/listener/calculator.py
@@ -95,10 +95,10 @@ class Calculator(commands.Cog, name="Calculator"):
                     await res.respond(
                         type=7,
                         embed=discord.Embed(
-                            title="{ctx.author.name}'s calculator",
+                            title=f"{ctx.author.name}'s calculator",
                             description=f"```The expression you entered has a result of : {expression}```",
                             color=discord.Colour.blurple(),
-                        ).footer(text="Restart calculator if you need to do anything else!"),
+                        ),
                         components=done,
                     )
 
