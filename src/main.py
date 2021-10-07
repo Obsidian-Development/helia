1c6fdd1d0fb0de7765663b1a7fbefd6669142985
pieckenst <rsncra@gmail.com>
2021-10-07 15:44:06 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import json
 import os
 os.system("ls -l; pip uninstall discord.py")
 os.system("ls -l; poetry remove discord.py") 
-os.system("ls -l; pip install git+https://github.com/Senarc-Studios/Orion.py.git")
+os.system("ls -l; pip install git+https://github.com/Senarc-Studios/Orion.py.git@Development")
 
 
 
