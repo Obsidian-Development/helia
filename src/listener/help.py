35f72c27425e436c1af764c341a93fec0ef17126
pieckenst <rsncra@gmail.com>
2021-10-10 20:07:04 +0300

    Like that i guess?

--- src/listener/help.py
+++ src/listener/help.py
@@ -10,7 +10,123 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: discord.Interaction):
+      print(f"{interaction.author.name} with ID {interaction.author.id} just clicked something in the select menu")
+      label = interaction.data.values[0]
+      if label.values[0] == "General":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "General":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":beginner: General",
+                        description=f"Here is the list of general commands we have \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Moderation":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "Moderation":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":hammer_pick: Moderation",
+                        description=f"Here is the list of moderation commands we have \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Utilities":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "Utilities":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":wrench: Utilities",
+                        description=f"Here is the list of utilities commands we have \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Music":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "Music":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":headphones: Music",
+                        description=f"Here is the list of music commands we have \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Preferences":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "Prefs":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":tools: Preferences",
+                        description=f"Here is the list of bot configuration commands \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+
+      if label.values[0] == "Welcome & Goodbye Messages":
+                descwelcgood = """
+                Here is the list of commands related to server join and leave messages
+                ```welcome - Displays this message```
+                .
+                ```welcome channel [#channel mention] - Set welcome channel```
+                .
+                ```welcome clear - Remove the set welcome channel```
+                .
+                ```welcome text {Optionally enter text - otherwise the default will be set} - Set welcome text```
+                .
+                ```goodbye - Displays this message```
+                .
+                ```goodbye channel [#channel mention] - Set goodbye channel```
+                .
+                ```goodbye clear - Remove the set goodbye channel```
+                .
+                ```goodbye text {Optionally enter text - otherwise the default will be set} - Set goodbye text```
+
+                """
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":wave: Welcome & Goodbye Messages",
+                        description=f"{descwelcgood}",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Other":
+                x = []
+                for y in self.bot.commands:
+                    if y.cog and y.cog.qualified_name == "Other":
+                        x.append(y.name)
+                formatlistprep = ":\n```.```".join(x)
+                await interaction.response.edit_message(
+                    
+                    embed=discord.Embed(
+                        title=":hourglass: Other",
+                        description=f"Here is the list of miscellaneous commands \n ```{formatlistprep}```",
+                    ).set_author(name="Help System"),
+                )
+      if label.values[0] == "Close":
+                await interaction.response.edit_message(
+                                          embed=embede,
+                                          view=None)
+
+      
     @commands.command(slash_interaction=True, message_command=True,description="Help Command")
     async def help(self, ctx):
         embed = discord.Embed(title="SELECTION TEST",
@@ -50,117 +166,7 @@ class Help(commands.Cog):
 
         
             
-        if selecter.values[0] == "General":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "General":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":beginner: General",
-                        description=f"Here is the list of general commands we have \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Moderation":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "Moderation":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":hammer_pick: Moderation",
-                        description=f"Here is the list of moderation commands we have \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Utilities":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "Utilities":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":wrench: Utilities",
-                        description=f"Here is the list of utilities commands we have \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Music":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "Music":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":headphones: Music",
-                        description=f"Here is the list of music commands we have \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Preferences":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "Prefs":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":tools: Preferences",
-                        description=f"Here is the list of bot configuration commands \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-
-        if selecter.values[0] == "Welcome & Goodbye Messages":
-                descwelcgood = """
-                Here is the list of commands related to server join and leave messages
-                ```welcome - Displays this message```
-                .
-                ```welcome channel [#channel mention] - Set welcome channel```
-                .
-                ```welcome clear - Remove the set welcome channel```
-                .
-                ```welcome text {Optionally enter text - otherwise the default will be set} - Set welcome text```
-                .
-                ```goodbye - Displays this message```
-                .
-                ```goodbye channel [#channel mention] - Set goodbye channel```
-                .
-                ```goodbye clear - Remove the set goodbye channel```
-                .
-                ```goodbye text {Optionally enter text - otherwise the default will be set} - Set goodbye text```
-
-                """
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":wave: Welcome & Goodbye Messages",
-                        description=f"{descwelcgood}",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Other":
-                x = []
-                for y in self.bot.commands:
-                    if y.cog and y.cog.qualified_name == "Other":
-                        x.append(y.name)
-                formatlistprep = ":\n```.```".join(x)
-                await interaction.response.edit_message(
-                    
-                    embed=discord.Embed(
-                        title=":hourglass: Other",
-                        description=f"Here is the list of miscellaneous commands \n ```{formatlistprep}```",
-                    ).set_author(name="Help System"),
-                )
-        if selecter.values[0] == "Close":
-                await interaction.response.edit_message(
-                                          embed=embede,
-                                          view=None)
+        
 
 
 def setup(bot):
