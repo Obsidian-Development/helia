1d4469e56757c2511a7910ee75501761161c1842
pieckenst <rsncra@gmail.com>
2021-10-30 22:41:11 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from listener.core.client import CoreClient
 from listener.prefs import Preferences
 from listener.utils import Config, Logger, Strings, Utils
 
-os.system("ls -l; pip uninstall -y discord.py")
+#os.system("ls -l; pip uninstall -y discord.py")
 os.system("ls -l; poetry remove discord.py")
 #os.system("ls -l; pip install discord")
 #os.system("ls -l; poetry add discord")
